460916016cfc350a6235f468ce9e8a1fa9b06c7f
jamal299 <hussain.08@gmail.com>
2021-09-03 21:08:49 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import data from './config';
 function App() {
+  const [formError, setFormError] = useState(true);
+  function submitHandler(data) {
+    console.log('formdata', data);
+  }
+  function inputHandler(e, validation) {
+    console.log(e.target.value, 'formdata');
+    console.log(validation, 'validation');
+    if (
+      validation &&
+      validation.required &&
+      validation.required === true &&
+      e.target.value.length === 0
+    ) {
+      setFormError(true);
+    } else {
+      setFormError(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form className='myform' onSubmit={submitHandler}>
+        <div className='loginheader'>Login form</div>
+        {data &&
+          data.map((val) => (
+            <div>
+              <span className='header'> {val.texts.header}</span>
+              <span className='inputs'>
+                {val.inputs.map((input) => (
+                  <div>
+                    <input
+                      type={input.type}
+                      onChange={(e) => inputHandler(e, input.validation)}
+                    />
+                  </div>
+                ))}
+              </span>
+            </div>
+          ))}
+        <button className='submit' disabled={formError} onClick={submitHandler}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
